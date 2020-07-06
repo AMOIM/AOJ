be9cb24e0a34e33a04e2565d8d631fb7c7d60526
kimcodingvv <junghun1997@naver.com>
2020-07-06 21:14:20 +0900

    Directory Structure

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
+const db = mongoose.connection;
 
-var db = mongoose.connection;
 db.on('error', console.error);
 db.once('open', function(){
   console.log("Connected to mongod server");
@@ -29,7 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
