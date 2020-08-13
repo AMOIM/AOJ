8f7a7691528fffa34b039ceb2a1f326b151369f4
Hwangsoomin <maum97@naver.com>
2020-08-14 03:09:12 +0900

    add vuejs-logger

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,20 @@ import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
+import VueLogger from 'vuejs-logger';
+const isProduction = process.env.NODE_ENV === 'production';
+ 
+const options = {
+    isEnabled: true,
+    logLevel : isProduction ? 'error' : 'debug',
+    stringifyArguments : false,
+    showLogLevel : true,
+    showMethodName : true,
+    separator: '|',
+    showConsoleColors: true
+};
+ 
+Vue.use(VueLogger, options);
 
 Vue.config.productionTip = false;
 
