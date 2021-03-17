aab6926a82d5ebb8e44e53631cac78119de5d526
Hwangsoomin <maum97@naver.com>
2021-03-17 15:37:06 +0900

    Update problem.route.js

--- JudgeWeb/backend/routes/problem.route.js
+++ JudgeWeb/backend/routes/problem.route.js
@@ -11,6 +11,7 @@ router.post('/create', authMiddlewareBackend('admin'), Problem.create);
 router.delete('/delete/:id', authMiddlewareBackend('admin'), Problem.delete);
 router.patch('/update/:id', authMiddlewareBackend('admin'), Problem.update);
 router.get('/testcase/:id', authMiddlewareBackend(), Problem.getTestcase);
+router.get('/testcase/example/:id', authMiddlewareBackend(), Problem.getTestcaseExample);
 router.put('/testcase/:id', authMiddlewareBackend('admin'), Problem.updateTestcase);
 router.delete('/testcase/:id', authMiddlewareBackend('admin'), Problem.deleteTestcase);
 
