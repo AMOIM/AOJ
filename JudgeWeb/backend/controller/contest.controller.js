9e9d88bb41fc40c0ce1ff0a48d40a5cb1f0d2c2e
Hwangsoomin <maum97@naver.com>
2021-03-19 20:53:18 +0900

    Update contest.controller.js

--- JudgeWeb/backend/controller/contest.controller.js
+++ JudgeWeb/backend/controller/contest.controller.js
@@ -10,7 +10,7 @@ export default class Contest {
             const result = await ContestService.update(req);
             return res.status(200).json(result);
         } catch(err) {
-            err.message = 'GET /contest/update\nController -> ' + err.message;
+            err.message = 'PUT /contest/update\nController -> ' + err.message;
             err.status = 400;
             next(err);
         }
