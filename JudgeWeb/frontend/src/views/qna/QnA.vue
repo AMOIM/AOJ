8aac72a042fa90b2bf226ea5a60fb486b4ec67e7
Soomin <43667316+Hwangsoomin@users.noreply.github.com>
2021-05-12 21:59:20 +0900

    Fix code for date format (#68)

--- JudgeWeb/frontend/src/views/qna/QnA.vue
+++ JudgeWeb/frontend/src/views/qna/QnA.vue
@@ -48,6 +48,7 @@ import componentReplyCreate from '@/components/notice/ReplyCreate';
 import sidebarComponent from '@/components/sidebar/SideBar';
 import problemSidebarComponent from '@/components/sidebar/ProblemSideBar';
 import {check} from '@/components/mixins/check';
+import moment from 'moment';
 
 export default {
     mixins:[check],
@@ -116,6 +117,9 @@ export default {
         try {
             let result = await this.$http.get(`/api/contest/notice?competitionNum=${this.competitionNum}&key=0`);
             this.notices = result.data;
+            for(let notice of this.notices) {
+                notice.date = moment().format('YYYY-MM-DD HH:mm:ss');
+            }
             try {
                 result = await this.$http.get(`/api/contest/${this.competitionNum}`);
                 this.problemList = result.data;
